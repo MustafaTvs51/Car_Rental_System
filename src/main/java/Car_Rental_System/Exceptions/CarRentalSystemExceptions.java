0b41ca355c359779e7d73781cd0e2c0d502aebc6
tvs51 <mustafa_Tavusbay@outlook.com>
2025-05-21 23:43:49 +0300

    Updated

--- src/main/java/Car_Rental_System/Exceptions/CarRentalSystemExceptions.java
+++
@@ -1,7 +0,0 @@
-package Car_Rental_System.Exceptions;
-
-public class CarRentalSystemExceptions extends RuntimeException {
-  public CarRentalSystemExceptions(String message) {
-    super(message);
-  }
-}
--- src/main/java/Car_Rental_System/Exceptions/ExceptionMessageConstant.java
+++
@@ -1,4 +0,0 @@
-package Car_Rental_System.Exceptions;
-
-public class ExceptionMessageConstant {
-}
